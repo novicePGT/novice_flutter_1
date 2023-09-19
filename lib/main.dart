1b3b101a818f74f3a283a8aa85cf474166e5c607
geuntae1 <dymnam@naver.com>
2023-09-19 22:49:05 +0900

    [BucketList]

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -11,7 +12,193 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: HomePage(),
+    );
+  }
+}
+
+/// 버킷 클래스
+class Bucket {
+  String title;
+  bool isComplete;
+
+  Bucket(this.title, this.isComplete); // 생성자
+}
+
+/// 홈 페이지
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  // property
+  List<Bucket> bucketList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("버킷 리스트"),
+      ),
+      body: bucketList.isEmpty
+          ? Center(child: Text("버킷 리스트를 작성해 주세요."))
+          : ListView.builder(
+              itemCount: bucketList.length,
+              itemBuilder: (context, index) {
+                Bucket bucket = bucketList[index];
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                  child: ListTile(
+                    title: Text(
+                      bucket.title,
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: bucket.isComplete ? Colors.grey : Colors.black,
+                        decoration: bucket.isComplete
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,
+                      ),
+                    ),
+                    trailing: IconButton(
+                      icon: Icon(
+                        CupertinoIcons.delete,
+                      ),
+                      onPressed: () {
+                        showDeleteDialog(context, index);
+                      },
+                    ),
+                    onTap: () {
+                      setState(() {
+                        bucket.isComplete = !bucket.isComplete;
+                      });
+                    },
+                  ),
+                );
+              }),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {
+          // + 버튼 클릭시 버킷 생성 페이지로 이동
+          String? job = await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (_) => CreatePage()),
+          );
+          if (job != null) {
+            setState(() {
+              Bucket newBucket = Bucket(job, false);
+              bucketList.add(newBucket);
+            });
+          }
+        },
+      ),
+    );
+  }
+
+  void showDeleteDialog(BuildContext context, int index) {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text("정말로 삭제하시겠습니까?"),
+            actions: [
+              TextButton(
+                child: Text("취소"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              TextButton(
+                child: Text(
+                  "삭제",
+                  style: TextStyle(color: Colors.red),
+                ),
+                onPressed: () {
+                  setState(() {
+                    bucketList.removeAt(index);
+                  });
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+}
+
+/// 버킷 생성 페이지
+class CreatePage extends StatefulWidget {
+  const CreatePage({Key? key}) : super(key: key);
+
+  @override
+  State<CreatePage> createState() => _CreatePageState();
+}
+
+class _CreatePageState extends State<CreatePage> {
+  // property
+  // TextField 컨트롤러: TextField 의 값을 가져올 때 사용한다.
+  TextEditingController textController = TextEditingController();
+  String? error;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("버킷리스트 작성"),
+        // 뒤로가기 버튼
+        leading: IconButton(
+          icon: Icon(CupertinoIcons.chevron_back),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // 텍스트 입력창
+            TextField(
+              controller: textController,
+              autofocus: true,
+              decoration: InputDecoration(
+                hintText: "하고 싶은 일을 입력하세요",
+                errorText: error,
+              ),
+            ),
+            SizedBox(height: 32),
+            // 추가하기 버튼
+            SizedBox(
+              width: double.infinity,
+              height: 48,
+              child: ElevatedButton(
+                child: Text(
+                  "추가하기",
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
+                onPressed: () {
+                  // 추가하기 버튼 클릭시
+                  String job = textController.text;
+                  if (job.isEmpty) {
+                    setState(() {
+                      error = "내용을 입력해주세요.";
+                    });
+                  } else {
+                    setState(() {
+                      error = null;
+                    });
+                    Navigator.pop(context, job);
+                  }
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
